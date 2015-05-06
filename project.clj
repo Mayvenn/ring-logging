d2c24a6b2291a31064618a531c3746dc448d49eb
Sheel Choksi <sheelchoksi@gmail.com>
2015-05-06 10:04:19 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logging "0.1.0"
+(defproject ring-logging "0.1.1-SNAPSHOT"
   :description "Logs ring requests and responses"
   :url "https://github.com/Mayvenn/ring-logging"
   :license {:name "Eclipse Public License"
