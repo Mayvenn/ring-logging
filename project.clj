b01b3e4ed852e8c13e84e1a2fb20a2f3d547fb4d
Greg Winn <gregw@mayvenn.com>
2015-07-24 16:40:20 -0700

    REDACTED

--- project.clj
+++ project.clj
@@ -4,8 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  
-                              
-                              
-                              
   :plugins [[s3-wagon-private "1.1.2"]])
