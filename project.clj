a14e9f8c5c5863a95424a56b998ad157c5382a1d
Jacob Maine <jacob@mayvenn.com>
2016-01-24 18:27:16 -0800

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logging "0.2.1"
+(defproject ring-logging "0.2.2-SNAPSHOT"
   :description "Logs ring requests and responses"
   :url "https://github.com/Mayvenn/ring-logging"
   :license {:name "Eclipse Public License"
