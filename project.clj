a7a07b9b22738b6e30f36f5433269bc26f446885
Greg Winn <gregw@mayvenn.com>
2017-10-13 14:00:37 -0700

    Set lein-release to use clojars [#nodeps]

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]]
+  :lein-release {:deploy-via :clojars}
   :deploy-repositories [["releases" :clojars]])
