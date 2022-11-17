777764bf6644c13c91e93c0ae8df6a102c964d87
Elizabeth Winn <elizabeth@mayvenn.com>
2022-11-17 15:39:31 -0800

    Version 0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logging "0.3.6-SNAPSHOT"
+(defproject ring-logging "0.3.6"
   :description "Logs ring requests and responses"
   :url "https://github.com/Mayvenn/ring-logging"
   :license {:name "Eclipse Public License"
